444fafe00febaf0667d8c37dce4e93781ad6b841
Luiz Carlos da Fonseca Jr <luiz.bruntec@gmail.com>
2024-01-22 22:45:47 -0300

    exemplo input2

--- mutation/projeto/resolvers/Mutation.js
+++ mutation/projeto/resolvers/Mutation.js
@@ -1,5 +1,22 @@
 const { usuarios, proximoId } = require('../data/db')
 
+function indiceUsuario(filtro) 
+{
+    if(!filtro) return -1
+    const { id, email } = filtro
+    if(id)
+    {
+        return usuarios
+            .findIndex(u => u.id === id)
+    }
+    else if(email)
+    {
+        return usuarios
+            .findIndex(u => u.email === email)
+    }
+    return -1
+}
+
 module.exports = {
     // { nome, email, idade }
     // novoUsuario(_, args) 
@@ -43,12 +60,21 @@ module.exports = {
         return novo
     },
 
-    excluirUsuario(_, { id }){
-        const i = usuarios
-            .findIndex(u => u.id === id)
+    // excluirUsuario(_, { id })
+    // {
+    //     const i = usuarios
+    //         .findIndex(u => u.id === id)
+    //     if(i < 0) return null
+    //     const excluidos = usuarios.splice(i, 1)
+    //         return excluidos ? excluidos[0] : null
+    // },
+
+    excluirUsuario(_, { filtro })
+    {
+        const i = indiceUsuario(filtro)
         if(i < 0) return null
         const excluidos = usuarios.splice(i, 1)
-            return excluidos ? excluidos[0] : null
+        return excluidos ? excluidos[0] : null
     },
 
     alterarUsuario(_, args) {
--- mutation/projeto/resolvers/Query.js
+++ mutation/projeto/resolvers/Query.js
@@ -1,13 +1,35 @@
 const { usuarios, perfis } = require('../data/db')
 
+function indiceUsuario(filtro) 
+{
+    if(!filtro) return -1
+    const { id, email } = filtro
+    if(id)
+    {
+        return usuarios
+            .findIndex(u => u.id === id)
+    }
+    else if(email)
+    {
+        return usuarios
+            .findIndex(u => u.email === email)
+    }
+    return -1
+}
+
 module.exports = {
     usuarios() {
         return usuarios
     },
-    usuario(_, { id }) {
-        const sels = usuarios
-            .filter(u => u.id === id)
-        return sels ? sels[0] : null
+    // usuario(_, { id }) {
+    //     const sels = usuarios
+    //         .filter(u => u.id === id)
+    //     return sels ? sels[0] : null
+    // },
+    usuario(_, { filtro }) {
+        const i = indiceUsuario(filtro)
+        if(i < 0) return null
+        return usuarios[i]
     },
     perfis() {
         return perfis
