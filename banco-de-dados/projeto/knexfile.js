38a2a12117c487f7b55a617abf3f2b1c0109f204
Luiz Carlos da Fonseca Jr <luiz.bruntec@gmail.com>
2024-01-23 23:38:47 -0300

    graphql BD

---
+++ banco-de-dados/projeto/knexfile.js
@@ -0,0 +1,48 @@
+// Update with your config settings.
+
+// /**
+//  * @type { Object.<string, import("knex").Knex.Config> }
+//  */
+// module.exports = {
+
+  // development: {
+  //   client: 'sqlite3',
+  //   connection: {
+  //     filename: './dev.sqlite3'
+  //   }
+  // },
+
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
+  // production: {
+  module.exports = {
+    client:  'mysql', //'postgresql',
+    connection: {
+      database: 'exercicios',
+      user:     'root',
+      password: ''
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+// };
